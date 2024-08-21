a5f1181576e8a13ac5cc33166f99a7f37c80ddab
Ganesh1724 <yashwanthganesh123@gmail.com>
2024-08-21 20:51:13 +0530

    my name is yashwanth

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        var a = num1+num2;
+        byte by = 127;
+        short sh = 550;
+
+        var a1 = by + sh;
+        long l = 5555l;
+
+        float f = 5.8f;
+        double d = 6.8;
+
+        char ch ='Y';
+        boolean b = true;
+        System.out.println( a1);
+    }
+}
